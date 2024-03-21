300c3be1d9737f4ad688d899f2b574c30ba1fe6a
jinizest <104010443+jinizest@users.noreply.github.com>
2024-03-22 00:15:15 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
+
 #!/bin/sh
 
 SHARE_DIR=/share/kocom
 
+# 기존 /share/kocom 디렉토리가 있으면 삭제합니다.
+if [ -d "$SHARE_DIR" ]; then
+    rm -rf "$SHARE_DIR"
+fi
+
+# 새로운 /share/kocom 디렉토리를 생성합니다.
 mkdir -p $SHARE_DIR
+
+# /kocom.conf 파일과 /kocom.py 파일을 /share/kocom 디렉토리로 이동시킵니다.
 mv /kocom.conf $SHARE_DIR
 mv /kocom.py $SHARE_DIR
 

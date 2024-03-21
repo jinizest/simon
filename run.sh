67632e04df1d3f4c477acb29dd35ac224f1196bf
jinizest <104010443+jinizest@users.noreply.github.com>
2024-03-21 22:37:10 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 SHARE_DIR=/share/kocom
 
 mkdir -p $SHARE_DIR
-mv /kocom.py $SHARE_DIR
 mv /kocom.conf $SHARE_DIR
+mv /kocom.py $SHARE_DIR
 
 echo "[Info] Run Kocom Wallpad with RS485!"
 cd $SHARE_DIR
